60b55bd4c6bd96111a6134be4041264b79fe87b8
Антон Выходцев <vykhodtsevai@gmail.com>
2019-07-30 22:53:02 +0300

    Вывод сео заголовков в постах

--- public/packages/zeusAdmin/js/ckeditor/config.js
+++ public/packages/zeusAdmin/js/ckeditor/config.js
@@ -7,6 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-
+    $.each(CKEDITOR.dtd.$removeEmpty, function (i, value) {
+        CKEDITOR.dtd.$removeEmpty[i] = false;
+    });
     config.allowedContent = true;
 };
